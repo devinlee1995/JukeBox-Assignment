b70d3f32c816d9d10e855e3220ef4a571a1e87b8
Devin <devinlee1995@yahoo.com>
2016-06-13 23:04:14 -0400

    finished juke css

--- script.js
+++ script.js
@@ -1,70 +1,114 @@
+//counter for the song array
 var count = 0;
-var total = 3;
+//total number of songs in the array
+var total = 4;
+
 function JukeBox(){
-	this.audio_array = [
+	this.audio_array = [ //the available songs
 		'Eat.mp3',
 		'Just.mp3',
-		'Sponsor.mp3'
+		'Sponsor.mp3',
+		'What2Do.mp3'
 	];
+	//setting the firsy song automatically
 	this.audie = document.getElementById("myAudio");
 	this.audie.src = this.audio_array[count];
+
+	//function for the playing a song
 	this.play = function() {
+		document.getElementById("status").innerHTML = "Playing"
 		document.getElementById("song_playing").innerHTML = this.audio_array[count];
+		//this code is for highlighing the current song loaded
+		for (var j = 0; j < total; j++) {
+			var sample = this.audio_array[j];
+			var array = sample.split(".");
+			document.getElementById(array[0]).style.color = "#000000";
+		}
+		var current = this.audio_array[count].split(".");
+		document.getElementById(current[0]).style.color = "#ff8fa3";
 	    this.audie.play();
 	};
+
+	//functino for pausing a song
 	this.pause = function() {
+		document.getElementById("status").innerHTML = "Paused"
 	    this.audie.pause();
 	};
+
+	//function for stopping the current song and loads the next one
+	//it does not play the next song
 	this.stop = function() {
+		document.getElementById("status").innerHTML = "Stopped"
 	    this.audie.pause();
 	    count = (count + 1)%total;
 	    this.audie.src = this.audio_array[count];
+	    //this code is for highlighing the current song loaded
+	    for (var j = 0; j < total; j++) {
+			var sample = this.audio_array[j];
+			var array = sample.split(".");
+			document.getElementById(array[0]).style.color = "#000000";
+		}
+		var current = this.audio_array[count].split(".");
+		document.getElementById(current[0]).style.color = "#ff8fa3";
 	    document.getElementById("song_playing").innerHTML = this.audio_array[count];
 	};
-	this.add = function(audioFile) {
-		if (audioFile != "") {
-			this.audio_array.push(audioFile);	
-		    total = total + 1;
-		    console.log(this.audio_array);
-		};
+
+	//function for the user to choose which song to play in the list
+	this.load_song = function(song_name) {
+		//this code is for highlighing the current song loaded
+		for (var j = 0; j < total; j++) {
+			var sample = this.audio_array[j];
+			var array = sample.split(".");
+			document.getElementById(array[0]).style.color = "#000000";
+		}
+  		document.getElementById(song_name).style.color = "#ff8fa3";
+		var file = song_name + ".mp3";
+	    this.audie.pause();
+	    for (var i = 0; i < total; i++) {
+	    	if (file == this.audio_array[i]) {
+	    		this.audie.src = this.audio_array[i];
+	    		count = i;
+	    	}
+	    }
+	    document.getElementById("song_playing").innerHTML = this.audio_array[count];
+	    this.audie.play();
 	};
 };
 
+//created the jukeBox object for the page
 var juk = new JukeBox();
 juk.play();
 
+//listener for the play button
 document.getElementById("Play").onclick=function(){
 	juk.play();
 };
 
+//listener for the pause button
 document.getElementById("Pause").onclick=function(){
 	juk.pause();
 };
 
+//listener for the stop button
 document.getElementById("Stop").onclick=function(){
 	juk.stop();
 };
 
-$(".activate-modal").click(function(e){
-    e.preventDefault(); //dont follow the link
-    $(".modal-wrapper").show(); //show our modal
-    //add the .modal-on class to <body>
-    $("body").addClass("modal-on");   
- });
+//The rest of these listeners are for loading a specific song
+document.getElementById("Eat").onclick=function(){
+	juk.load_song("Eat");
+};
 
-$(".add").click(function(e){
-    e.preventDefault();
-    $(".modal-wrapper").hide();
-    $("body").removeClass("modal-on");
-    var new_song = document.getElementById('song_name').value;
-    juk.add(new_song);
-  });
+document.getElementById("Just").onclick=function(){
+	juk.load_song("Just");
+};
 
-$(".exit").click(function(e){
-	e.preventDefault();
-	//basically the opposite of activate
-	$(".modal-wrapper").hide();
-	$("body").removeClass("modal-on");
-});
+document.getElementById("Sponsor").onclick=function(){
+	juk.load_song("Sponsor");
+};
+
+document.getElementById("What2Do").onclick=function(){
+	juk.load_song("What2Do");
+};
 
 
